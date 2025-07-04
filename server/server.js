3de428ba648e05e900738df6ce0c455ba53ed8d4
Pranjal-045 <23ee01022@iitbbs.ac.in>
2025-07-04 10:07:37 +0530

    Update server.js

--- server/server.js
+++ server/server.js
@@ -37,13 +37,13 @@ app.use('/api', quizRoutes);
 app.use('/api', shareRoutes);
 
 // Serve static files in production
-if (process.env.NODE_ENV === 'production') {
-  app.use(express.static(path.join(__dirname, '../client/build')));
+// if (process.env.NODE_ENV === 'production') {
+//   app.use(express.static(path.join(__dirname, '../client/build')));
   
-  app.get('*', (req, res) => {
-    res.sendFile(path.join(__dirname, '../client/build/index.html'));
-  });
-}
+//   app.get('*', (req, res) => {
+//     res.sendFile(path.join(__dirname, '../client/build/index.html'));
+//   });
+// }
 
 // Error handling middleware
 app.use((err, req, res, next) => {
